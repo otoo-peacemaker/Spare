09971d9d886baa564bb76c7ad82a872f25c7211f
peacemaker-otoo <potoo@idscorpgh.com>
2023-04-15 08:28:53 +0000

    modification

--- app/src/main/java/com/peacemaker/android/spare/ui/util/Resource.kt
+++ app/src/main/java/com/peacemaker/android/spare/ui/util/Resource.kt
@@ -3,7 +3,7 @@ package com.peacemaker.android.spare.ui.util
 
 data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
     companion object {
-        fun <T> success(data: T): Resource<T> =
+        fun <T> success(data: T?): Resource<T> =
             Resource(status = Status.SUCCESS, data = data, message = null)
 
         fun <T> error(data: T?, message: String): Resource<T> =
