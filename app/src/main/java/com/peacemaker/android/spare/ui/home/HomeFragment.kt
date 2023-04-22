816da4b81d179c81ef3e0ffa2c436256a5a06726
peacemaker-otoo <potoo@idscorpgh.com>
2023-04-22 03:06:14 +0000

    modified

--- app/src/main/java/com/peacemaker/android/spare/ui/home/HomeFragment.kt
+++ app/src/main/java/com/peacemaker/android/spare/ui/home/HomeFragment.kt
@@ -1,12 +1,24 @@
 package com.peacemaker.android.spare.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import com.peacemaker.android.spare.R
 import com.peacemaker.android.spare.data.items
 import com.peacemaker.android.spare.databinding.FragmentHomeBinding
@@ -17,12 +29,12 @@ class HomeFragment : BaseFragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val dataSets = arrayListOf<IBarDataSet?>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?): View {
         val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         // Get the action bar instance from the parent activity
@@ -42,9 +54,30 @@ class HomeFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupAutoCompleteTextView(binding.autoCompleteTextView, listOf("Week","Days","Month","Year")){
 
+        val autoCompleteTextView = binding.autoCompleteTextView
+        val dataSpinner = listOf("Week","Days","Month","Year")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, dataSpinner)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        autoCompleteTextView.adapter = adapter
+        autoCompleteTextView.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                Toast.makeText(requireContext(), "Selected item: $selectedItem", Toast.LENGTH_SHORT).show()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // do nothing
+            }
         }
+
+        dataSets.clear()
+        dataSets.add(barDataSet((R.color.light_blue_A200), arrayListOf(BarEntry(3f, 48f)), "January"))
+        dataSets.add(barDataSet((R.color.light_blue_A400), arrayListOf(BarEntry(2f, 70f)), "March"))
+        dataSets.add(barDataSet((R.color.light_blue_600), arrayListOf(BarEntry(1f, 68.0F)), "April"))
+        dataSets.add(barDataSet((R.color.md_theme_dark_error), arrayListOf(BarEntry(2f, 70f)), "Feb"))
+        dataSets.add(barDataSet((R.color.light_blue_900), arrayListOf(BarEntry(1f, 68.0F)), "May"))
+        setBarChart(binding.bankTransactionChart, dataSets, arrayListOf("January", "March", "April","Feb","May"))
+
     }
 
     override fun onDestroyView() {
