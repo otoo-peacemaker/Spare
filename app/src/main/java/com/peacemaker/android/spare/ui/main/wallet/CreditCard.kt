5ea2f2a122d2fbb62ea2fa414e369c58ecd7459d
peacemaker-otoo <potoo@idscorpgh.com>
2023-04-29 15:29:46 +0000

    carousel

--- app/src/main/java/com/peacemaker/android/spare/ui/main/wallet/CreditCard.kt
+++ app/src/main/java/com/peacemaker/android/spare/ui/main/wallet/CreditCard.kt
@@ -1,8 +1,8 @@
 package com.peacemaker.android.spare.ui.main.wallet
 
-data class CreditCard (
-    val cardType:String,
-val cardHolderName:String,
-val cardNumber: String,
-val cardExpiryDate:String
+data class CreditCard(
+    val cardType: String,
+    val cardHolderName: String,
+    val cardNumber: String,
+    val cardExpiryDate: String
 )
